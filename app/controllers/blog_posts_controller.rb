14297241ed4e7400827f34f36476d23979a18e0b
EvanArianenda <athariqevanarianenda@alumni.undip.ac.id>
2024-05-13 20:52:35 +0700

    Add edit blog post feature

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -13,9 +13,22 @@ class BlogPostsController < ApplicationController
     @blog_post = BlogPost.new
   end
 
+  def edit
+    @blog_post = BlogPost.find(params[:id])
+  end
+
+  def update
+    @blog_post = BlogPost.find(params[:id])
+    if @blog_post.update
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def create
-    @blog_post = BlogPost.new(blog_post_params)
-    if @blog_post.save
+    @blog_post = BlogPost.find(params[:id])
+    if @blog_post.update(blog_post_params)
       redirect_to @blog_post
     else
       render :new, status: :unprocessable_entity
